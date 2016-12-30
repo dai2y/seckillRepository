4630ef77225133a7d1854a0f511276bdfbef562e
dy_git <695805297@qq.com>
2016-12-30 12:25:15 +0800

    生成数据实体

---
+++ src/main/java/dao/SecKillEntry.java
@@ -0,0 +1,82 @@
+package dao;
+
+import java.util.Date;
+
+/**
+ * 秒杀库存实体类
+ * Created by Auser on 2016/12/30.
+ */
+public class SecKillEntry {
+
+    private Long seckillId;
+
+    private String name;
+
+    private Long number;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Date createTime;
+
+    public Long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(Long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SecKillEntry{" +
+                "seckillId=" + seckillId +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
