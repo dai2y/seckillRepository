adc1b1b5daff35e0f789ce97c363193d34f8bf6e
dy_git <695805297@qq.com>
2017-01-04 15:41:33 +0800

    测试用例提交

--- src/test/java/ContralTest.java
+++ src/test/java/ContralTest.java
@@ -1,5 +1,22 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Auser on 2016/12/28.
  */
 public class ContralTest {
+    @Test
+    public void firstTest() {
+        assertEquals(1, 1);
+    }
+    @Test
+    public void testJDK(){
+        List<String> list = Arrays.asList("Hello", "World!");
+        list.stream().forEach(System.out::println);
+    }
+
 }
